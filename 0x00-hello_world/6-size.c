b3db7e44877da4a36e81e495fe10b43757bc402b
Samuel Udemagwuna <samueludemagwuna@gmail.com>
2022-12-09 13:58:19 -0800

    resolved

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf(Size of a char : 1 byte(s)
+Size of an int : 4 byte(s)
+Size of a long : 4 byte(s)
+Size of a long long : 8 byte(s)
+Size of a float : 4 byte(s));
+return (0);
+}
