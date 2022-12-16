8096ef93541d63654d52130b3e586c7b2bb0e26c
Samuel Udemagwuna <samueludemagwuna@gmail.com>
2022-12-16 02:36:12 -0800

    final debug

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- * @positive_or_negative - Entry point
- * Description:print value of @i status; zero,
- * positive or negative
- * Return: Always 0 (Success)
+ * positive_or_negative - Entry point
+ * @i: status will bezero,positive or negative
  */
-
-void positive_or_negagive(int i)
+void positive_or_negative(int i)
 {
 	if (i == 0)
 	{
 		printf("%d is zero\n", i);
-	}
-	else if (i > 0)
+	} else if (i > 0)
 	{
 		printf("%d is positive\n", i);
-	}
-	else if (i < 0)
+	} else if (i < 0)
 	{
 		printf("%d is negative\n", i);
 	}
