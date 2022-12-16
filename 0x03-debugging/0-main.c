0cf6d1b62f536949927ffa2bdaab603dd1ebf127
Samuel Udemagwuna <samueludemagwuna@gmail.com>
2022-12-15 19:04:40 -0800

    first debug test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer
+ * is positive or negative
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98 % 4;
+	positiv or negative(i);
+
+	return (0);
+}
