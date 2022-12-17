7379f4fcaf21541a1d15762f8a62288b90e32f5d
Samuel Udemagwuna <samueludemagwuna@gmail.com>
2022-12-17 02:11:26 -0800

    sweet phrase

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,15 @@
  */
 
 void print_most_numbers(void)
-{
-	int i;
+
+{ int i;
+	b
 
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i + '0')
+			_putchar(i + '0');
 		}
 	}
 
