888b2b18e8ddff4f3d5dc0b1022deb996dd37693
Samuel Udemagwuna <samueludemagwuna@gmail.com>
2022-12-16 18:29:58 -0800

    extrac hours

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _isupper - checks for uppercase
+ * _isupper - checks for uppercase
  * character
  * @c: the character to be checked
  * Return: 1 if c uppercase, 0 otherwise
