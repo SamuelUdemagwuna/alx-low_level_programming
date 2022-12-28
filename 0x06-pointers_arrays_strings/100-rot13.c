706190c4794faa3bb75c88213d3f0f3d73fcf661
Samuel Udemagwuna <samueludemagwuna@gmail.com>
2022-12-28 09:51:29 -0800

    hot fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,9 +15,9 @@ char *rot13(char *s)
 	char data1[] =
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] =
-	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++;)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *rot13(char *);
 void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r int size_r);
 void print_buffer(char *b, int size);
