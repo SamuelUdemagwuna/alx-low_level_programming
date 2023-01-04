77c713f6a46f785e8209769f363d7de47bd29ea0
Samuel Udemagwuna <samueludemagwuna.gmail.com>
2023-01-04 12:06:39 -0800

    final run on recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,8 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
  * comparator - compares each character of the string.
@@ -21,13 +20,13 @@ int _strlen_recursion(char *s)
  * Return: .
  */
 
-int comparator(char *s, int n1, int n)
+int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
-			return(1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
+			return (1);
+return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
@@ -42,5 +41,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
