1a9196f2dba9e4509945c4a70ca8123e3398f614
Samuel Udemagwuna <samueludemagwuna.gmail.com>
2023-01-28 04:15:14 -0800

    header files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of filr compiled from
+ * @i: name of file
+ *
+ * Return: Always (0) success.
+ */
+
+int main(void)
+{
+	char* i;
+
+	i = 2-main *.c;
+	printf("%s\n", i);
+	return (0);
+}
